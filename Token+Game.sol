6aec3964f756bc1576f5caad91d9c33440c00a24
NovationScripts <150507216+NovationScripts@users.noreply.github.com>
2024-09-23 00:03:12 +0300

    Update Token+Game.sol

--- Token+Game.sol
+++ Token+Game.sol
@@ -1,8 +1,13 @@
-  // SPDX-License-Identifier: MIT 
+    // SPDX-License-Identifier: MIT 
 
     pragma solidity ^0.8.0;
     import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
    
+    // Интерфейс для взаимодействия с внешним контрактом
+   interface IExternalContract {
+   function getFlags(address _player) external view returns (uint256[] memory);
+   }
+
 
    contract MultimillionaireToken is ReentrancyGuard {
    // Constants for fund distribution
@@ -25,6 +30,7 @@
 
    address[] public playersArray; // Массив для хранения всех адресов игроков
 
+   address[] public externalContracts; // Array to store addresses of external contracts
    // Contract state variables
    address public owner; // Owner of the contract
    uint256 public totalReferralEarnings = 0; // Total referral earnings
@@ -56,6 +62,9 @@
 
 
    // /////////////////////////////////////////////////////////////
+
+   
+
    // Modifier to ensure that only the contract owner can call certain functions
     modifier onlyOwner() {
     // Check that the message sender is the owner of the contract
@@ -70,6 +79,42 @@
     require(!players[msg.sender].hasFinished, "Player has finished the game");
     _; // Continue execution of the function
     }
+
+
+     modifier checkRegistrationFlagsWithExternalContracts(address _player) {
+    // Проверяем, что у игрока есть хотя бы один флаг для регистрации
+    require(players[_player].registrationFlags.length > 0, "No registration flags in player's array");
+
+    // Проверяем флаги из всех внешних контрактов
+    for (uint256 i = 0; i < externalContracts.length; i++) {
+        IExternalContract externalContract = IExternalContract(externalContracts[i]);
+        uint256[] memory externalFlags = externalContract.getFlags(_player);
+
+        // Проверяем наличие каждого разрешенного флага игрока в массиве внешних флагов
+        for (uint256 j = 0; j < players[_player].registrationFlags.length; j++) {
+            bool flagFound = false;
+            for (uint256 k = 0; k < externalFlags.length; k++) {
+                if (players[_player].registrationFlags[j] == externalFlags[k]) {
+                    flagFound = true;
+                    break;
+                }
+            }
+            require(flagFound, "One or more registration flags not found in external contract");
+        }
+
+        // Проверяем, что запрещенные флаги отсутствуют
+        for (uint256 j = 0; j < players[_player].prohibitedRegistrationFlags.length; j++) {
+            for (uint256 k = 0; k < externalFlags.length; k++) {
+                require(
+                    players[_player].prohibitedRegistrationFlags[j] != externalFlags[k],
+                    "Player has prohibited flags"
+                );
+            }
+        }
+    }
+    _; 
+    }
+
 
     // /////////////////////////////////////////////////////
 
@@ -223,6 +268,9 @@
     uint256 referralWithdrawals; // Variable added to track the total amount of withdrawals made by referrals
     uint256 lastDepositTime; // Time of the player's last deposit;
     uint256 nextPayoutAttemptTime; // Time of the player's next payout attempt
+    uint256[] flags; // Массив флагов игрока после прохождения игры
+    uint256[] registrationFlags; // Массив флагов для доступа к регистрации
+    uint256[] prohibitedRegistrationFlags; // Запрещенные флаги к регистрации
     }
 
 
@@ -231,7 +279,7 @@
     // /////////////////////////////////////////////////////////
 
     // Функция для регистрации игрока
-    function register(address _referrer) external {
+    function register(address _referrer) external checkRegistrationFlagsWithExternalContracts(msg.sender) {
     // Проверяем, что игрок не зарегистрирован
     require(players[msg.sender].referrer == address(0), "Player already registered");
 
@@ -598,19 +646,109 @@
     }
 
 
-    // ///////////////////////////////////////////////////////////
+
+  
+
+
+    function setDepositAmount(uint256 index, uint256 newAmount) external onlyOwner {
+    require(index < depositAmounts.length, "Index out of bounds");
+    require(newAmount > 0, "Deposit amount must be greater than zero");
+    depositAmounts[index] = newAmount;
+    }
+
+
 
     
+
+    // ///////////////////////////////////////////////////////////
+
+      // Добавляем флаг игроку
+    function addFlag(address _player, uint256 _flag) public {
+        Player storage player = players[_player];
+        player.flags.push(_flag); // Добавляем новый флаг
+    }
+
+    // Удаление флага по индексу с заменой на последний элемент массива
+    function removeFlag(address _player, uint256 flagIndex) public {
+        Player storage player = players[_player];
+        require(flagIndex < player.flags.length, "Invalid flag index");
+
+        // Перемещаем последний элемент на место удаляемого
+        player.flags[flagIndex] = player.flags[player.flags.length - 1];
+        player.flags.pop(); // Удаляем последний элемент массива
+    }
+
+    // Функция для просмотра флагов, доступна только если player.hasFinished == true
+    function getAvailableFlags() public view returns (uint256[] memory) {
+        Player storage player = players[msg.sender];
+
+        // Проверяем, что игрок завершил все депозиты
+        require(player.hasFinished, "Player has not finished the game");
+
+        // Возвращаем все флаги игрока
+        return player.flags;
+    }
+
+
+
+    
+
+
+
+
+     // Добавляем внешний контракт в массив
+    function addExternalContract(address _externalContract) public {
+        externalContracts.push(_externalContract);
+    }
+
+    // Добавляем флаг игроку в массив registrationFlags
+    function addRegistrationFlag(address _player, uint256 _flag) public {
+    Player storage player = players[_player];
+    player.registrationFlags.push(_flag); // Добавляем новый флаг в массив registrationFlags
+    }
+
+    // Удаление флага из массива registrationFlags по индексу с заменой на последний элемент массива
+    function removeRegistrationFlag(address _player, uint256 flagIndex) public {
+    Player storage player = players[_player];
+    require(flagIndex < player.registrationFlags.length, "Invalid flag index");
+
+    // Перемещаем последний элемент на место удаляемого
+    player.registrationFlags[flagIndex] = player.registrationFlags[player.registrationFlags.length - 1];
+    player.registrationFlags.pop(); // Удаляем последний элемент массива
+    }
+
+
+
+
+
+    
+
+
+
+    // Добавляем запрещенный флаг игроку в массив prohibitedRegistrationFlags
+    function addProhibitedRegistrationFlag(address _player, uint256 _flag) public {
+    Player storage player = players[_player];
+    player.prohibitedRegistrationFlags.push(_flag); // Добавляем новый флаг в массив prohibitedRegistrationFlags
+    }
+
+    // Удаление флага из массива prohibitedRegistrationFlags по индексу с заменой на последний элемент массива
+    function removeProhibitedRegistrationFlag(address _player, uint256 flagIndex) public {
+    Player storage player = players[_player];
+    require(flagIndex < player.prohibitedRegistrationFlags.length, "Invalid flag index");
+
+    // Перемещаем последний элемент на место удаляемого
+    player.prohibitedRegistrationFlags[flagIndex] = player.prohibitedRegistrationFlags[player.prohibitedRegistrationFlags.length - 1];
+    player.prohibitedRegistrationFlags.pop(); // Удаляем последний элемент массива
+    }
+
+
+
+
+    // ////////////////////////////////////////////////////////////////////
+
      
-
-    function getTotalPlayers() public view returns(uint256) {
-    return totalPlayerCount;
-    }
-
-    // Функция для получения текущего значения ratioMultiplier
-    function getRatioMultiplier() external view returns (uint256) {
-    return ratioMultiplier;
-    }
+    
+    
 
     // Функция для получения данных о текущем депозите игрока
     function getCurrentDepositData() external view returns (uint256 depositAmount, uint256 depositIndex, uint256 budget) {
@@ -651,18 +789,7 @@
 
 
 
-    // Функция для получения информации о конкретном игроке
-    function getPlayerInfo(address _playerAddress) external view returns(uint256 currentDepositIndex, uint256 depositsCompleted, bool hasFinished) {
-    // Получаем данные игрока из хранилища
-    Player storage player = players[_playerAddress];
-
-    // Возвращаем текущий индекс депозита, количество завершённых депозитов и статус завершения
-    return (
-        player.depositIndex,    // Текущий индекс депозита (с какого депозита начнётся следующий)
-        player.depositIndex,    // Количество завершённых депозитов (совпадает с индексом)
-        player.hasFinished      // Статус завершения игры (все депозиты выполнены)
-    );
-    }
+   
 
 
     // Функция для получения бюджетов всех депозитов
@@ -681,44 +808,6 @@
 
 
 
-    //  //////////////////////////////////////////////
-
-    // Function to get the contract commission percentage.
-    function getContractEarningsPercentage() public pure returns (uint256) {
-    // Return the constant value of the contract commission.
-    return contractCommission;
-    }
-
-    // Function to get the total earnings of the contract.
-    function getTotalContractEarnings() public view returns (uint256) {
-    // Return the total earnings accumulated by the contract.
-    return contractEarnings;
-    }
-
-
-     // Function to retrieve a player's referral earnings.
-    function getReferralEarnings() public view returns (uint256) {
-    // Returns the amount of referral earnings for the message sender.
-    return players[msg.sender].referralEarnings;
-    }
-    
-    // Function to get the total referral earnings.
-    function getTotalReferralEarnings() public view returns (uint256) {
-    // Return the total amount of referral earnings.
-    return totalReferralEarnings;
-    }
-
-    // Function to get referral earnings by wallet address
-    function getReferralEarningsByWallet(address referrer) public view returns (uint256) {
-    // Return the referral earnings of the specified referrer address.
-    return players[referrer].referralEarnings;
-    }
-
-    // Function to get referral withdrawals by wallet address
-    function getReferralWithdrawalsByWallet(address referrer) public view returns (uint256) {
-    // Return the total amount of referral withdrawals of the specified referrer address.
-    return players[referrer].referralWithdrawals;
-    }
 
 
     // //////////////////////////////////////////////////////
